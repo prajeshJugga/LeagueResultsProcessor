05e8d32af9ecc8f7a52547bea3e0446639d8b2a6
prajeshJugga <prajesh.jugga@gmail.com>
2022-02-10 23:50:31 +0200

    added implementation for file reader

---
+++ league_results_processor/file_objects/simple_game_results_line.py
@@ -0,0 +1,6 @@
+from league_results_processor.file_objects.ifile_object import ifile_object
+
+class simple_game_results_line(ifile_object):
+    """docstring for ClassName."""
+    def __init__(self):
+        super(simple_game_results_line, self).__init__()
--- league_results_processor/file_readers/ifile_reader.py
+++ league_results_processor/file_readers/ifile_reader.py
@@ -1,11 +1,8 @@
 from typing import List
 from league_results_processor.file_objects.ifile_object import ifile_object
 
-class ifile_reader():
+class ifile_reader:
     """docstring for ClassName."""
-
-    def __init__(self):
-        pass
 
     def read_file(self, file_path) -> List[ifile_object]:
         with open(file_path, 'r') as reader:
